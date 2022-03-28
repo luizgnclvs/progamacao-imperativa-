0ea9017b905d278553daa0f1e9e4b31a648906dc
Luiz Gonçalves <94024386+luizgnclvs@users.noreply.github.com>
2022-03-28 03:18:23 -0300

    Update version1.java

--- revisao/gradeCalculator/src/version1.java
+++ revisao/gradeCalculator/src/version1.java
@@ -2,32 +2,32 @@ import java.util.Scanner;
 
 public class Version1 {
     
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         
-        Scanner read = new Scanner(System.in);
-			
-	double grade1, grade2, grade3;
-	String status;
+		Scanner read = new Scanner(System.in);
 
-	System.out.println("Insira sua primeira nota:");
-	grade1 = read.nextDouble();
+		double grade1, grade2, grade3;
+		String status;
 
-	System.out.println("\nInsira sua segunda nota:");
-	grade2 = read.nextDouble();
+		System.out.println("Insira sua primeira nota:");
+		grade1 = read.nextDouble();
 
-	System.out.println("\nInsira sua terceira nota:");
-	grade3 = read.nextDouble();
+		System.out.println("\nInsira sua segunda nota:");
+		grade2 = read.nextDouble();
 
-	double finalGrade = (grade1 + grade2 + grade3)/3;
+		System.out.println("\nInsira sua terceira nota:");
+		grade3 = read.nextDouble();
 
-	if (finalGrade >= 7) {
-		status = "APROVADO POR MÉDIA";
-	} else if (finalGrade >=5) {
-		status = "APROVADO";
-	} else {
-		status = "REPROVADO";
-	}
+		double finalGrade = (grade1 + grade2 + grade3)/3;
 
-	System.out.printf("\nMédia: %.2f \nSituação: %s", finalGrade, status);
-    }
+		if (finalGrade >= 7) {
+			status = "APROVADO POR MÉDIA";
+		} else if (finalGrade >=5) {
+			status = "APROVADO";
+		} else {
+			status = "REPROVADO";
+		}
+
+		System.out.printf("\nMédia: %.2f \nSituação: %s", finalGrade, status);
+	   	}
 }
