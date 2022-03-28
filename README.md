48f4b24bb2c4cdb74c5dbab7b5e54a70211fddc9
luizgnclvs <luizgnclvss@gmail.com>
2022-03-27 22:21:20 -0300

    grade calculator v1

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Programação Imperativa
 
 Exercícios propostos pelo professor ao decorrer da disciplina de Programação Imperativa do curso de Sistemas para Internet, ofertada pela UNICAP (Universidade Católica de Pernambuco) em 2022.1
+
+### Índice
+
+- Exercícios de revisão
+- Vetores #1
+- Vetores #2
+- Vetores #3
---
+++ revisao/gradeCalculator/src/version1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class version1 {
+    
+    public static void main(String[] args) {
+        
+        Scanner read = new Scanner(System.in);
+		
+		double grade1, grade2, grade3;
+		String status;
+		
+		System.out.println("Insira sua primeira nota:");
+		grade1 = read.nextDouble();
+		
+		System.out.println("\nInsira sua segunda nota:");
+		grade2 = read.nextDouble();
+		
+		System.out.println("\nInsira sua terceira nota:");
+		grade3 = read.nextDouble();
+		
+		double finalGrade = (grade1 + grade2 + grade3)/3;
+		
+		if (finalGrade >= 7) {
+			status = "APROVADO POR MÉDIA";
+		} else if (finalGrade >=5) {
+			status = "APROVADO";
+		} else {
+			status = "REPROVADO";
+		}
+		
+		System.out.printf("\nMédia: %.2f \nSituação: %s", finalGrade, status);
+    }
+}
